1dbeb1259c5c1913b43a3508a30970a365a79fcc
Lu <2711236168@qq.com>
2020-06-25 20:23:11 +0800

    day2

---
+++ Code4.cpp
@@ -0,0 +1,24 @@
+//
+// Created by LuMengLi on 2020/6/23.
+//
+
+#include <crtdbg.h>
+
+/**
+ * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
+请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+你可以假设 nums1 和 nums2 不会同时为空。
+**/
+//double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
+//    int num=nums1Size+nums2Size;
+//    //奇数个
+//    if(num%2==0){
+//        if (nums1Size>=nums2Size){
+//            while
+//        } else{
+//
+//        }
+//    } else if(num%2!=0){
+//
+//    }
+//}
